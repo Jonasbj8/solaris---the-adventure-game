bd50c6e870d37613754ca63e797303315229ae71
Jonasbj8 <91563923+Jonasbj8@users.noreply.github.com>
2021-09-28 20:24:44 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,252 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "MageLeft":return img`
+. . . . f f f f f f . . . . . . 
+. . . f 8 f 8 8 8 8 f f . . . . 
+. . f 8 5 8 8 8 8 8 8 f f . . . 
+. . f 8 8 8 8 f f 8 8 8 f . . . 
+. f 8 8 8 8 8 8 8 f f f f . . . 
+. f 8 8 f f f f 8 8 8 8 f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 8 8 8 e d d 4 . . . . . 
+. . . f 8 8 8 e d d e . . . . . 
+. . . f 5 5 e e e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`;
+            case "image3":
+            case "BasicLeft":return img`
+. . . . f f f f f f . . . . . . 
+. . . f 2 f e e e e f f . . . . 
+. . f 2 2 2 f e e e e f f . . . 
+. . f e e e e f f e e e f . . . 
+. f e 2 2 2 2 e e f f f f . . . 
+. f 2 e f f f f 2 2 2 e f . . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 2 2 2 e d d 4 . . . . . 
+. . . f 2 2 2 e d d e . . . . . 
+. . . f 5 5 4 f e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`;
+            case "image5":
+            case "MageRight":return img`
+. . . . . . 8 8 8 8 8 5 . . . . 
+. . . . f 8 8 8 8 8 8 8 f . . . 
+. . . f 8 8 8 8 8 8 8 8 8 f . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 f . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 8 8 8 f . . . 
+. . . . . e d d e 8 8 8 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`;
+            case "image4":
+            case "MageBehind":return img`
+. . . . . 5 8 8 8 8 . . . . . . 
+. . . . 5 5 8 8 8 8 8 8 . . . . 
+. . . f 8 8 8 8 8 8 8 8 f . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. f f e f 8 f e e f 8 f e f f . 
+5 f e e f f e e e e f e e e f . 
+5 . f e e e e e e e e e e f . . 
+e e . f e e e e e e e e f . . . 
+. e e 4 f f f f f f f f 4 e . . 
+. . 4 d f 8 8 8 8 8 8 f d 4 . . 
+. . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image1":
+            case "Hero_Melee":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "MageRightMove":
+            case "anim1":return [img`
+. . . . . . f 8 8 8 5 5 . . . . 
+. . . . f f 8 8 8 5 5 8 f . . . 
+. . . f 8 8 8 8 8 8 8 8 8 f . . 
+. . . f 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 8 8 8 f . . . 
+. . . . . e d d e 8 8 8 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`, img`
+. . . . . . . 8 8 8 5 5 . . . . 
+. . . . . . 8 8 8 5 5 8 . . . . 
+. . . . f 8 8 8 8 5 8 8 f . . . 
+. . . f f 8 8 8 8 8 8 8 8 f . . 
+. . . 8 8 8 8 8 8 8 8 8 8 f . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e e e d d d f . . . 
+. . . . . f 4 d d e 4 e f . . . 
+. . . . . f e d d e 8 8 f . . . 
+. . . . f f f e e f 5 5 f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . f f . . . f f f . . . 
+`, img`
+. . . . . . f 8 8 8 8 5 5 . . . 
+. . . . f 8 8 8 8 5 5 5 f . . . 
+. . . f f 8 8 8 8 5 8 8 8 f . . 
+. . . f 8 8 8 8 8 8 8 8 8 f . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 8 8 8 f . . . 
+. . . . . e d d e 8 8 8 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`, img`
+. . . . . . . . 8 8 5 5 . . . . 
+. . . . . . f 8 8 5 5 8 . . . . 
+. . . . f 8 8 8 8 5 8 8 8 . . . 
+. . . f f 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . 4 d d e 4 4 4 e f . . . 
+. . . . e d d e 8 8 8 8 f . . . 
+. . . . f e e f 4 4 5 5 f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . f f . . . f f f . . . 
+`];
+            case "MageWalkLeftAnimation":
+            case "anim2":return [img`
+. . . . 8 8 8 8 8 f . . . . . . 
+. . . f 8 8 8 8 8 8 f f . . . . 
+. . f 8 8 8 8 8 8 8 8 8 f . . . 
+. . 8 8 8 8 8 8 8 8 8 8 f . . . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 . . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 8 8 8 e d d 4 . . . . . 
+. . . f 8 8 8 e d d e . . . . . 
+. . . f 5 e e e e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`, img`
+. . . . . 8 8 8 . . . . . . . . 
+. . . . 8 8 8 8 8 . . . . . . . 
+. . . f 8 8 8 8 8 8 8 f . . . . 
+. . f 8 8 8 8 8 8 8 8 8 f . . . 
+. . f 8 8 8 8 8 8 8 8 8 8 . . . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d e e e e e f . . . 
+. . . f e 4 e d d 4 f . . . . . 
+. . . f 8 8 e d d e f . . . . . 
+. . f f 5 5 f e e f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . f f f . . . f f . . . . . 
+`, img`
+. . . . f 8 8 8 f f . . . . . . 
+. . . f 8 8 8 8 8 8 f f . . . . 
+. . f 8 8 8 8 8 8 8 8 f f . . . 
+. . f 8 8 8 8 8 8 8 8 8 f . . . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 . . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e e f f . . . . 
+. . . f 8 8 8 e d d 4 . . . . . 
+. . . f 8 8 8 e d d e . . . . . 
+. . . f 5 5 4 f e e f . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`, img`
+. . . . . 8 8 8 . . . . . . . . 
+. . . . 8 8 8 8 8 f . . . . . . 
+. . . f 8 8 8 8 8 8 f f . . . . 
+. . f 8 8 8 8 8 8 8 8 f f . . . 
+. . f 8 8 8 8 8 8 8 8 8 f . . . 
+. f 8 8 8 8 8 8 8 8 8 8 8 . . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. f f f e e e f f f f f f f . . 
+. f e e 4 4 f b e 4 4 e f f . . 
+. . f e d d f 1 4 d 4 e e f . . 
+. . . f d d d d 4 e e e f . . . 
+. . . f e 4 4 4 e d d 4 . . . . 
+. . . f 8 8 8 8 e d d e . . . . 
+. . f f 5 5 4 4 f e e f . . . . 
+. . f f f f f f f f f f . . . . 
+. . . f f f . . . f f . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
